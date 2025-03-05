3dcdd0f2f3bf6ffebc2215de709656f1343ad9e0
Lucas Rios <lucasriosdev@gmail.com>
2025-03-04 21:07:43 -0300

    adicionado a classe fillter-all no botão de filtras (html) adicionado o filter no JavaScript para filtrar os burguer vegano e add o toLocaleString no JavaScript para formatar a moeda em BRL.

--- scripts.js
+++ scripts.js
@@ -1,7 +1,20 @@
+
 const list = document.querySelector('ul')
 const buttonShowAll = document.querySelector('.show-all')
 const buttonMapAll = document.querySelector('.map-all')
 const sumAll = document.querySelector('.sum-all')
+const filterAll = document.querySelector('.filter-all')
+
+function formatCurrency(value) {
+    const newValue = value.toLocaleString('pt-br', { 
+        style: 'currency', 
+        currency: 'BRL' 
+    })
+
+    return newValue
+
+}
+
 
 
 function showAll(productsArray) {
@@ -12,7 +25,7 @@ function showAll(productsArray) {
         <li>
                 <img src=${product.src}>
                 <p>${product.name}</p>
-                <p class="item-price"> R$ ${product.price}</p>
+                <p class="item-price">${formatCurrency(product.price)}</p>
             </li>
     
         `
@@ -36,18 +49,26 @@ function mapAllItems() {
 function sumAllItems() {
     const totalValue = menuOptions.reduce((acc, curr) => acc + curr.price, 0) // --> CURR - current
 
-        list.innerHTML = `
+    list.innerHTML = `
 
         
         <li>
-             <p>O valor total dos itens é de R$ ${totalValue} Reais.</p>
+             <p>O valor total dos itens é de: ${formatCurrency(totalValue)}.</p>
          </li>
     `
-    
+
 }
+
+function filterAllItems() {
+    const filterJustVegan = menuOptions.filter((product) => product.vegan)
+
+    showAll(filterJustVegan)
+}
+
 
 
 
 buttonShowAll.addEventListener('click', () => showAll(menuOptions))
 buttonMapAll.addEventListener('click', mapAllItems)
 sumAll.addEventListener('click', sumAllItems)
+filterAll.addEventListener('click', filterAllItems)
